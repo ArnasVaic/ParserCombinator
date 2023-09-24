3ba07c24800ff8c92162872781f50c18ff3222d3
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2023-09-24 19:06:54 +0300

    Initial

---
+++ ParserCombinator/Core/Either.cs
@@ -0,0 +1,45 @@
+﻿namespace ParserCombinator.Core;
+
+using System.Diagnostics;
+
+public class Either<TLeft, TRight>
+{
+    private readonly TLeft? _left;
+
+    private readonly TRight? _right;
+
+    private Either(TLeft? left, TRight? right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public static Either<L, R> From<L, R>(L? left) => new(left, default);
+
+    public static Either<L, R> From<L, R>(R? right) => new(default, right);
+
+    public Either<TLeft, TResult> Map<TResult>(Func<TRight, TResult> func)
+    {
+        if(_right is not null)
+            return From<TLeft, TResult>(func(_right));
+        
+        if(_left is not null)
+            return From<TLeft, TResult>(_left);
+
+        throw new UnreachableException("Something went really wrong.");
+    }
+
+    public void Map<TResult>(Action<TRight> action)
+    {
+        if(_right is not null)
+        {
+            action(_right);
+            return;
+        }
+
+        if(_left is not null)
+            return;
+
+        throw new UnreachableException("Something went really wrong.");
+    }
+}
